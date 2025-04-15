9db15a96e17c3575487d922076b2af4d8a30f28a
Kauê José <kauejose2211@gmail.com>
2025-04-14 21:56:59 -0300

    Ajustes finais binarios service

--- src/app/service/binarios-game/binarios-game.service.ts
+++ src/app/service/binarios-game/binarios-game.service.ts
@@ -9,58 +9,51 @@ export class BinariosGameService {
   saveProgress(
     userResponses: string[], 
     score: number, 
-    challengeState: any, 
     currentActivity: number, 
     expectedResponse: string, 
     level: number
   ): void {
-    const progressKey = level === 1 ? 'level1Progress' : 'level2Progress';
-  
+    const progressKey = `level${level}Progress`; 
+    
     const progressData = {
       userResponses: userResponses || [], 
       score: score, 
-      challengeState: challengeState, 
       currentActivity: currentActivity, 
-      expectedResponse: expectedResponse 
+      expectedResponse: expectedResponse,
     };
-  
-    
-    localStorage.setItem(progressKey, JSON.stringify(progressData));
-    console.log('Progresso e respostas salvos no localStorage:', progressData);
-  }  
 
+    localStorage.setItem(progressKey, JSON.stringify(progressData));
+    console.log(`Progresso do Nível ${level} e respostas salvos no localStorage:`, progressData);
+  }
   
   getProgress(level: number): any {
-    
-    const progressKey = level === 1 ? 'level1Progress' : 'level2Progress';
+    const progressKey = `level${level}Progress`;
     const savedProgress = localStorage.getItem(progressKey);
-    console.log('Progresso recuperado do localStorage:', savedProgress);
+    console.log(`Progresso do Nível ${level} recuperado do localStorage:`, savedProgress);
     return savedProgress ? JSON.parse(savedProgress) : null;
   }
 
-  
   clearProgress(): void {
-    localStorage.removeItem('level1Progress');
-    localStorage.removeItem('level2Progress');
+    for (let i = 1; i <= 7; i++) {
+      localStorage.removeItem(`level${i}Progress`);  
+    }
     
     console.log('Progresso de todos os níveis removido');
   }  
 
   private calculateScore(userResponses: string[], expectedResponse: string): number {
     let score = 0;
-  
+
     userResponses.forEach((response) => {
       if (response === expectedResponse) {  
         score++;  
       }
     });
-  
+
     return score;  
   }   
-  
-  
+
   addAnswer(userResponses: string[], newAnswer: string): string[] {
-    
     if (!userResponses.includes(newAnswer)) {
       userResponses.push(newAnswer);
     }
