62c0e884f25e140d8a1186a802c9be718ca4ad96
Kauê José <kauejose2211@gmail.com>
2025-04-29 11:44:16 -0300

    Level 7 implementado
    Adição do botão Home em todas as telas do nível, ajustes de responsividade no CSS das telas 2 e 3, implementação das estrelas na tela 2 com cálculo e localStorage, e ajustes na tela 3 para exibição das estrelas e mensagem de feedback.

--- src/app/components/levels/seven/screen-two-level-seven/screen-two-level-seven.component.ts
+++ src/app/components/levels/seven/screen-two-level-seven/screen-two-level-seven.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastService } from '../../toast.service';
-import { Question } from 'src/app/models/question.model';
-import { QuestionsService } from 'src/app/service/question/questions.service';
-import { SessionStorageService } from 'src/app/service/session-storage/session-storage-service.service';
+import { BinariosGameService } from 'src/app/service/binarios-game/binarios-game.service';
 
 @Component({
   selector: 'app-screen-two-level-seven',
@@ -11,82 +9,100 @@ import { SessionStorageService } from 'src/app/service/session-storage/session-s
   styleUrls: ['./screen-two-level-seven.component.css']
 })
 export class ScreenTwoLevelSevenComponent implements OnInit {
-
-  btnClass1: string = "";
-  btnClass2: string = "";
-  btnClass3: string = "";
-  btnClass4: string = "";
-  btnClass5: string = "";
+  userResponses: string[] = [];  
+  score: number = 0;  
+  currentActivity: number = 1; 
+  question: string = 'O que pode acontecer quando você coloca um 0 à direita de um número binário?';
+  expectedResponse: string = 'O número é dobrado'; 
+  answers: string[] = ['É dividido por 2', 'Nada acontece', 'É multiplicado por 10', 'O número é dobrado'];
+  btnClass1: string = '';
+  btnClass2: string = '';
+  btnClass3: string = '';
+  btnClass4: string = '';
 
   imageRef: number | undefined;
   attempts: number = 0;
 
-  idUser: string = this.sessionStorageService.getItem('userID') || 'Default Data';
-  idApp: string = "WEB-BINARIOS 1.0";
-  phaseActivity: string = "6";
-  numberActivity: string = "1";
-  typeOfQuestion: string = "MULTIPLA ESCOLHA";
-  expectedResponse: string = "O número é dobrado";
+  idUser: string = 'Default Data'; 
+  idApp: string = 'WEB-BINARIOS 1.0';
+  phaseActivity: string = '7';
+  numberActivity: string = '1';
+  typeOfQuestion: string = 'MULTIPLA ESCOLHA';
   dateResponse: Date;
 
-  question: string = "O que pode acontecer quando você coloca um 0 à direita de um número binário?";
-  answers: string[] = ["É dividido por 2", "Nada acontece", "É multiplicado por 10", "O número é dobrado"];
-
   constructor(
-    private router: Router, 
+    private router: Router,
     public toastService: ToastService,
-    private questionsService: QuestionsService, 
-    private sessionStorageService: SessionStorageService,
+    private binariosGameService: BinariosGameService
   ) {
     this.dateResponse = new Date();
   }
 
   ngOnInit(): void {
-    this.answers.sort(() => Math.random() - 0.5);
     this.imageRef = 1;
-  }
-
-  processQuestionResponse(userResponse: string, isCorrect: boolean): void {
-    const question: Question = new Question(this.idUser, this.idApp, this.phaseActivity, this.numberActivity, userResponse, this.expectedResponse, isCorrect, this.dateResponse, this.typeOfQuestion);
-    this.questionsService.saveResponseQuestion(question).subscribe(
-      response => {
-        console.log("Question saved successfully:", response);
-      },
-      error => {
-        console.error("Error saving question:", error);
-      }
-    );
+    this.toastService.clear();
+    this.answers.sort(() => Math.random() - 0.5);
   }
 
   processAnswer(answer: string, btn: number): void {
-    if (answer == this.expectedResponse) {
-      if (this.numberActivity == "1" && this.imageRef === 1) {
-        this.handleFirstAnswer(btn);
-      } else if (this.numberActivity == "2" && this.imageRef === 2) {
-        this.handleSecondAnswer(btn);
-      } else if (this.numberActivity == "3" && this.imageRef === 3) {
-        this.handleThirdAnswer(btn);
-      }
+    this.userResponses.push(answer);
+    const isCorrect = answer === this.expectedResponse;
 
-      this.processQuestionResponse(answer, true);
-      this.toastService.show('Parabéns!'); 
-      setTimeout(() => {
-        this.toastService.clear(); 
-      }, 1000);
-
+    if (isCorrect) {
+      this.buttonClass(btn, true);
+      this.toastService.show('Parabéns!', 'success');
     } else {
       this.handleIncorrectAnswer(answer, btn);
     }
+
+    const progress = {
+      userResponses: this.userResponses,
+      currentActivity: this.currentActivity,
+    };
+
+    this.saveProgress(progress, 7, this.expectedResponse, this.currentActivity);
+
+    if (isCorrect) {
+      setTimeout(() => {
+        if (this.currentActivity === 1) {
+          this.handleFirstAnswer(btn);
+        } else if (this.currentActivity === 2) {
+          this.handleSecondAnswer(btn);
+        } else if (this.currentActivity === 3) {
+          this.handleThirdAnswer(btn);
+        }
+      }, 1000);
+    }
+  }
+
+  saveProgress(progress: any, level: number, expectedResponse: string, currentActivity: number): void {
+    const progressWithScore = {
+      ...progress,
+      score: this.score,
+    };
+
+    this.binariosGameService.saveProgress(
+      progressWithScore.userResponses,  
+      progressWithScore.score,          
+      currentActivity,                  
+      expectedResponse,                
+      level                             
+    );
+  }
+
+  handleIncorrectAnswer(answer: string, btn: number): void {
+    this.buttonClass(btn, false);
+    this.toastService.show('Tente outra vez.', 'error');
+    this.attempts += 1;
   }
 
   handleFirstAnswer(btn: number): void {
     this.buttonClass(btn, true);
     setTimeout(() => {
-      this.toastService.clear();
-      this.answers = ["9", "18", "20", "11"];
-      this.question = "Qual número deve estar no lugar da (?) na imagem?";
-      this.numberActivity = "2";
-      this.expectedResponse = "18";
+      this.answers = ['9', '18', '20', '11'];
+      this.question = 'Qual número deve estar no lugar da (?) na imagem?';
+      this.currentActivity = 2;  
+      this.expectedResponse = '18';
       this.imageRef = 2;
       this.answers.sort(() => Math.random() - 0.5);
     }, 1000);
@@ -95,44 +111,55 @@ export class ScreenTwoLevelSevenComponent implements OnInit {
   handleSecondAnswer(btn: number): void {
     this.buttonClass(btn, true);
     setTimeout(() => {
-      this.toastService.clear();
-      this.answers = ["128", "64", "256", "32"];
-      this.question = "Um computador precisa de 7 bits para representar todos os caracteres. Isto permite representar até quantos caracteres?";
-      this.numberActivity = "3";
-      this.expectedResponse = "128";
+      this.answers = ['128', '64', '256', '32'];
+      this.question = 'Um computador precisa de 7 bits para representar todos os caracteres. Isto permite representar até quantos caracteres?';
+      this.currentActivity = 3;  
+      this.expectedResponse = '128';
       this.imageRef = 3;
+      this.answers.sort(() => Math.random() - 0.5);
     }, 1000);
   }
 
   handleThirdAnswer(btn: number): void {
     this.buttonClass(btn, true);
     setTimeout(() => {
-      this.toastService.clear();
-      this.router.navigate(['fase-7-3']);
+      this.calculateFinalScore();  
+      this.router.navigate(['fase-7-3'], { queryParams: { score: this.score } });  
     }, 1000);
   }
 
-  handleIncorrectAnswer(answer: string, btn: number): void {
-    this.buttonClass(btn, false);
-    this.toastService.show('Tente outra vez.', 'error'); 
-    this.attempts += 1;
-    this.processQuestionResponse(answer, false);
-  }  
+  calculateFinalScore(): void {
+    const totalQuestions = 3; 
+    const totalAttempts = this.userResponses.length; 
+    const correctAnswers = totalQuestions; 
+
+    const totalErrors = totalAttempts - correctAnswers;
+    const errorPercentage = totalErrors / totalAttempts;
+
+    const maxStars = 5;
+    const score = Math.max(1, Math.round((1 - errorPercentage) * maxStars));
+
+    this.score = score;
+
+    console.log('Score final:', this.score);  
+  }
 
   buttonClass(button: number, status: boolean): void {
-    const buttonClass = status ? "correct" : "incorrect";
-    if (button === 1) this.btnClass1 = buttonClass;
-    else if (button === 2) this.btnClass2 = buttonClass;
-    else if (button === 3) this.btnClass3 = buttonClass;
-    else if (button === 4) this.btnClass4 = buttonClass;
-    else if (button === 5) this.btnClass5 = buttonClass;
-
-    setTimeout(() => {
-      this.btnClass1 = "";
-      this.btnClass2 = "";
-      this.btnClass3 = "";
-      this.btnClass4 = "";
-      this.btnClass5 = "";
-    }, 1000);
+    if (button === 1) {
+      this.btnClass1 = status ? 'correct' : 'incorrect';
+      setTimeout(() => { this.btnClass1 = ''; }, 1000);
+    }
+    if (button === 2) {
+      this.btnClass2 = status ? 'correct' : 'incorrect';
+      setTimeout(() => { this.btnClass2 = ''; }, 1000);
+    }
+    if (button === 3) {
+      this.btnClass3 = status ? 'correct' : 'incorrect';
+      setTimeout(() => { this.btnClass3 = ''; }, 1000);
+    }
+    if (button === 4) {
+      this.btnClass4 = status ? 'correct' : 'incorrect';
+      setTimeout(() => { this.btnClass4 = ''; }, 1000);
+    }
   }
 }
